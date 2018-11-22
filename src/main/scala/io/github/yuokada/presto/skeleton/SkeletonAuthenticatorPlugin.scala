3ed699e998d35d10358a17afacf3a8035cea1bac
Yukihiro Okada <callistoiv+git@gmail.com>
2018-11-23 01:08:36 +0900

    Codecleanup

--- src/main/scala/io/github/yuokada/presto/skeleton/SkeletonAuthenticatorPlugin.scala
+++ src/main/scala/io/github/yuokada/presto/skeleton/SkeletonAuthenticatorPlugin.scala
@@ -5,6 +5,7 @@ import com.facebook.presto.spi.security.PasswordAuthenticatorFactory
 import com.google.common.collect.ImmutableList
 
 class SkeletonAuthenticatorPlugin extends Plugin {
-  override def getPasswordAuthenticatorFactories: ImmutableList[PasswordAuthenticatorFactory] =
+  override def getPasswordAuthenticatorFactories: ImmutableList[PasswordAuthenticatorFactory] = {
     ImmutableList.builder[PasswordAuthenticatorFactory].add(new SkeletonAuthenticatorFactory).build
+  }
 }
